177e4f461e1e771de8118563513b9b92002e0bdf
andela-fadebayo <fiyinfoluwa.adebayo@andela.co>
2015-01-15 09:20:35 +0100

    fixed cross-origin problem

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ app.use('/api/users', usersRoutes);
 //Allow for cross domain and access to others
 var allowCrossDomain = function (request, response, next) {
   response.header('Access-Control-Allow-Origin', '*');
-  response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  response.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, X-Requested-With');
+  response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  response.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
   next();
 };
